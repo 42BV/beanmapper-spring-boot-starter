73a0f75e55423709ead3b8f46c566932825513a4
Bas de Vos <bas.de.vos@42.nl>
2017-03-21 16:09:22 +0100

    Added MergedFormConfig.

--- src/main/java/io/beanmapper/autoconfigure/BeanMapperAutoConfig.java
+++ src/main/java/io/beanmapper/autoconfigure/BeanMapperAutoConfig.java
@@ -1,22 +1,31 @@
 package io.beanmapper.autoconfigure;
 
+import java.util.Collections;
+import java.util.List;
+
 import org.hibernate.proxy.HibernateProxy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.repository.core.EntityInformation;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import io.beanmapper.BeanMapper;
 import io.beanmapper.config.BeanMapperBuilder;
 import io.beanmapper.spring.converter.IdToEntityBeanConverter;
 import io.beanmapper.spring.unproxy.HibernateAwareBeanUnproxy;
+import io.beanmapper.spring.web.MergedFormMethodArgumentResolver;
+import io.beanmapper.spring.web.converter.StructuredJsonMessageConverter;
 
 @Configuration
 @ConditionalOnMissingBean(BeanMapper.class)
@@ -29,7 +38,7 @@ public class BeanMapperAutoConfig {
     @Autowired
     private BeanMapperProperties props;
     @Autowired
-    private ApplicationContext applicationContext;
+    public ApplicationContext applicationContext;
 
     @Bean
     public BeanMapper beanMapper() {
@@ -38,6 +47,27 @@ public class BeanMapperAutoConfig {
                 .setBeanUnproxy(new HibernateAwareBeanUnproxy())
                 .addProxySkipClass(Enum.class)
                 .addConverter(new IdToEntityBeanConverter(applicationContext))
+                // add extra converters
                 .build();
+    }
+
+    @Configuration
+    @ConditionalOnWebApplication
+    static class MergedFormWebConfig extends WebMvcConfigurerAdapter {
+
+        @Autowired
+        private MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter;
+        @Autowired
+        private BeanMapper beanMapper;
+        @Autowired
+        private ApplicationContext applicationContext;
+
+        @Override
+        public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+            argumentResolvers.add(new MergedFormMethodArgumentResolver(
+                    Collections.singletonList(new StructuredJsonMessageConverter(mappingJackson2HttpMessageConverter)),
+                    beanMapper,
+                    applicationContext));
+        }
     }
 }
