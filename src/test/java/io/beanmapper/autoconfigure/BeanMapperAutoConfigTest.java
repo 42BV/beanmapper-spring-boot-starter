9a3f7eb863677b812aa51dd261bc38a144098b52
Arjan Vlek <arjan.vlek@42.nl>
2018-06-28 11:15:14 +0200

    Made beanmapper-spring-boot-starter compatible with Spring Boot 2 and Spring Framework 5: - Updated dependencies - Updated changed imports (breaks backwards compatibility)

--- src/test/java/io/beanmapper/autoconfigure/BeanMapperAutoConfigTest.java
+++ src/test/java/io/beanmapper/autoconfigure/BeanMapperAutoConfigTest.java
@@ -20,9 +20,9 @@ import io.beanmapper.spring.web.MergedFormMethodArgumentResolver;
 
 import org.junit.After;
 import org.junit.Test;
+import org.springframework.boot.autoconfigure.http.HttpMessageConvertersAutoConfiguration;
 import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
-import org.springframework.boot.autoconfigure.web.HttpMessageConvertersAutoConfiguration;
-import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mock.web.MockServletContext;
