af8ffc404fadb7878af95a0a37fad76e9beda463
Marcus Talbot <marcus.talbot@42.nl>
2023-05-09 09:27:23 +0200

    Updated ApplicationTest
    - Fixed test in preparation of release.

--- src/test/java/io/beanmapper/autoconfigure/ApplicationTest.java
+++ src/test/java/io/beanmapper/autoconfigure/ApplicationTest.java
@@ -1,14 +1,18 @@
 package io.beanmapper.autoconfigure;
 
 import io.beanmapper.BeanMapper;
+import io.beanmapper.config.CoreConfiguration;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-@SpringBootTest
+@SpringBootTest(classes = {BeanMapperAutoConfig.class, CoreConfiguration.class, BeanMapper.class})
 public class ApplicationTest {
+
+
 
     @Autowired
     private BeanMapper beanMapper;
